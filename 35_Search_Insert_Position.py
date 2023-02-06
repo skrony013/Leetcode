a5fb3d90c763f3f8a4b0605c980faae3451f1879
MD RONY AHMED <mdronyahmed013@gmail.com>
2023-02-06 11:59:16 +0600

    Update.. binary search

--- 35_Search_Insert_Position.py
+++ 35_Search_Insert_Position.py
@@ -2,14 +2,13 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
-            if nums[m] < target:
+            if target > nums[m]:
                 l = m + 1
-            elif nums[m] > target:
+            elif target < nums[m]:
                 r = m - 1
             else:
                 return m
-        return l
